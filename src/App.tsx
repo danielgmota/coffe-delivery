71e80e73d25253d4452848de7ed85ac86a1f44e1
Daniel Mota <danielgmrs@outlook.com>
2023-09-13 08:42:49 -0300

    fix: routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
-import { BrowserRouter } from "react-router-dom";
-import { Router } from "./Router";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Router";
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
+      <RouterProvider router={router} />
 
       <GlobalStyle />
     </ThemeProvider>
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,18 +2,23 @@ import { HeaderComponent, LocationHeader } from "./Header.styles";
 import img_logo from "../../assets/logo.svg";
 import { MapPin } from "phosphor-react";
 import { Cart } from "../cart";
+import { Link } from "react-router-dom";
 
 export function Header() {
   return (
     <>
       <HeaderComponent>
-        <img src={img_logo} />
+        <Link to="/">
+          <img src={img_logo} />
+        </Link>
         <div>
           <LocationHeader>
             <MapPin />
             Rio de Janeiro, RJ
           </LocationHeader>
-          <Cart />
+          <Link to="/">
+            <Cart />
+          </Link>
         </div>
       </HeaderComponent>
     </>
